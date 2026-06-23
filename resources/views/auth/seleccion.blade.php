8801d86c8abb5ed1df97e115da36191da30f73a5
CRDavid02 <73392625@continental.edu.pe>
2026-06-22 23:38:49 -0500

    Update seleccion.blade.php

--- resources/views/auth/seleccion.blade.php
+++ resources/views/auth/seleccion.blade.php
@@ -12,8 +12,8 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap">
-  <link rel="stylesheet" href="{{ asset('css/variables.css') }}">
-  <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('css/variables.css') }}">
+  <link rel="stylesheet" href="{{ secure_asset('css/admin.css') }}">
 </head>
 <body>
 
